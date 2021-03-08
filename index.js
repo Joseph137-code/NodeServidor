5f0b8f1e984a28e28d6e3b58ff3a12de86492fda
Jose Sierra <segjsierra@gmail.com>
2021-03-08 11:54:56 -0300

    ok

--- index.js
+++ index.js
@@ -16,10 +16,7 @@ const app = express();
 conectarDB();
 
 //Habilitar Cors
-const opcionesCors = {
-    origin: process.env.FRONTEND_URL
-}
-app.use( cors(opcionesCors) );
+app.use( cors() );
 
 //Puerto de la App
 const port = process.env.PORT || 4000;
